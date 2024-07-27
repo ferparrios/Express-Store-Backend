d86d34e7fa7e9fc3bf5e38437147e99cab03ab65
Fernando Paredes Rios <fprios112@gmail.com>
2024-07-27 00:18:15 -0500

    subiendo archivos para vercel

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const routerApi = require('./routes')
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 // const whitelist = ['http://localhost:8081', 'exp://192.168.1.39:8081']
@@ -22,11 +22,11 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola Mundo')
 })
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola Mundo en la nueva ruta')
 })
 
